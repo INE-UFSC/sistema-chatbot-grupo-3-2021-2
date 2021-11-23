b6c2267816f79a88cffb2cccc7f433d3681106a6
Thiago Bewiahn <thiagoaugustobewiahn@gmail.com>
2021-11-23 10:54:05 -0300

    Update Bot.py

--- Bots/Bot.py
+++ Bots/Bot.py
@@ -4,18 +4,17 @@ from abc import ABC, abstractmethod
 import random as r
 
 class Bot(ABC):
+    def __init__(self, nome: str):
+        self.__nome = nome
+        self.__comandos = {}
 
-    def __init__(nome,):
-        self.nome = nome
-        self.comandos = {}
-
-    #nao esquecer o decorator
+    @property
     def nome(self):
-        pass
+        return self.__nome
 
-    #nao esquecer o decorator
-    def nome(nome):
-        pass
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
 
     def mostra_comandos(self):
         pass
