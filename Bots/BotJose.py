77936b9cbe8572a17e2e01402779d26364d546fc
kalleoOuriques <90047255+kalleoOuriques@users.noreply.github.com>
2021-11-30 11:41:33 -0300

    Add files via upload
    BotJose redondo

--- Bots/BotJose.py
+++ Bots/BotJose.py
@@ -4,10 +4,10 @@ class BotJose(Bot):
     def __init__(self,nome):
         self.__nome = nome
         self.__comandos = {
-            "conselho para os estudos": "José analisa suas notas\nDesistir é para os fracos, o ideal é nem tentar",
-            "conselho amoroso": "José analisa seu Tinder\nNunca é tarde para um novo fracasso",
+            "conselho para os estudos": "José analisa suas nota\nJosé diz: Desistir é para os fracos, o ideal é nem tentar",
+            "conselho amoroso": "José analisa seu Tinder\nJosé diz: Nunca é tarde para um novo fracasso",
             "conselho para a carreira": "José te entrega um guia de como se comportar numa entrevista\nRegra 1: chame o empregador de 'meu parça', é contrato na certa",
-            }
+            "adeus": "José diz: Vamos esquecer os erros do passado, meu amigo, e focar nos erros do futuro. Adeus, até vista"}
 
     @property
     def nome(self):
@@ -22,19 +22,20 @@ class BotJose(Bot):
         return self.__comandos
 
     def apresentacao(self):
-        return "Mensagem de apresentação: Olá, eu sou o José, seu bot conselheiro"
- 
+        msg = f'Mensagem de apresentação: Olá, eu sou o {self.__nome}, seu bot conselheiro'
+        return msg
     def mostra_comandos(self):
         return self.__comandos
-        
+    
     def executa_comando(self,cmd):
         try:
             return self.__comandos[cmd]
         except:
-            print("Não sei")
+            print("Você está louco?")
 
     def boas_vindas(self):
         return "José diz: Que bom que você me escolheu! Espero que eu possa te ajudar"
+    
 
     def despedida(self):
-        return "Vamos esquecer os erros do passado, meu amigo, e focar nos erros do futuro. Adeus, até vista"
+        return "José diz: Vamos esquecer os erros do passado, meu amigo, e focar nos erros do futuro. Adeus, até vista"
