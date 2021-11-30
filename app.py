60f080f6c364c6ad68a43b4fea3313c798e48139
Thiago Bewiahn <thiagoaugustobewiahn@gmail.com>
2021-11-30 09:34:04 -0300

    remove bot feliz

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
 from Bots.BotMarombeiro import BotMarombeiro
-from Bots.BotFeliz import BotFeliz
 from Bots.BotGamer import BotGamer
 from Bots.BotEspelhado import BotEspelhado
 
