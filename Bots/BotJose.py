58c38c2a83f56634139924303b755a8c47fb792f
Thiago Bewiahn <thiagoaugustobewiahn@gmail.com>
2021-11-30 11:17:43 -0300

    Update BotJose.py

--- Bots/BotJose.py
+++ Bots/BotJose.py
@@ -1,13 +1,13 @@
 from Bots.Bot import Bot
 
-class BotTriste(Bot):
+class BotJose(Bot):
     def __init__(self,nome):
         self.__nome = nome
         self.__comandos = {
-            '1': 'Oláa!!',
-            '2': 'Como você está? :)',
-            '3': 'Pode me ajudar?',
-            '4': 'Tchau'}
+            "1": "conselho para os estudos",
+            "2": "conselho amoroso",
+            "3": "conselho para a carreira",
+            "4": "adeus"}
 
     @property
     def nome(self):
@@ -15,31 +15,36 @@ class BotTriste(Bot):
 
     @nome.setter
     def nome(self, nome):
-        self.nome = nome
-        
+        self.__nome = nome
+
     @property
     def comandos(self):
         return self.__comandos
 
     def apresentacao(self):
-        return f'Meu nome, infelizmente, é {self.__nome}...'
-    
+        print("Mensagem de apresentação: Olá, eu sou o José, seu bot conselheiro")
+ 
+    def mostra_comandos(self):
+        print("1 - conselho para os estudos")
+        print("2 - conselho amoroso")
+        print("3 - conselho para a carreira")
     def executa_comando(self,cmd):
-        print('     --> Eu te respondo: ', end='')
-        if cmd == '1':
-            print('Oi...')
-        elif cmd == '2':
-            print('Queria sentir algo para te responder...')
-        elif cmd == '3':
-            print('Até posso, mas acho que não ajudaria muito de qualquer jeito...')
-        elif cmd == '4':
-            print('Tchau...')
+        if cmd == 1:
+            print("José analisa suas notas")
+            print("José diz: Desistir é para os fracos, o ideal é nem tentar")
+        elif cmd == 2:
+            print("José analisa seu Tinder")
+            print("José diz: Nunca é tarde para um novo fracasso")
+        elif cmd == 3:
+            print("José te entrega um guia de como se comportar numa entrevista")
+            print("Regra 1: chame o empregador de 'meu parça', é contrato na certa")
+        elif cmd == 4:
+            print("José diz: Vamos esquecer os erros do passado, meu amigo, e focar nos erros do futuro. Adeus, até vista")
         else:
-            print('Você não digitou um comando válido e estou muito triste :(')
-            
+            print("Comando inexistente")
 
     def boas_vindas(self):
-        return f'--> {self.nome} diz: Você tinha que escolher logo o mais inútil?'
+        print("José diz: Que bom que você me escolheu! Espero que eu possa te ajudar")
 
     def despedida(self):
-        return f'--> {self.nome} diz: Finalmente você vai se livrar de mim!'
+        print("José diz: Vamos esquecer os erros do passado, meu amigo, e focar nos erros do futuro. Adeus, até vista")
