f1f5bb1acfcfec1ffa1660b1255f41be908dcc03
Bernardo Arruda <bernardo.a@grad.ufsc.br>
2021-11-30 11:33:20 -0300

    adicionando botminerin e botmanezinho no app.py

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from Bots.BotGamer import BotGamer
 from Bots.BotEspelhado import BotEspelhado
 from Bots.BotZangado import BotZangado
 from Bots.BotTriste import BotTriste
+from Bots.BotManezinho import BotManezinho
+from Bots.BotMinerin import BotMinerin
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotMarombeiro("Bambam"), BotGamer("Ninja"), BotEspelhado("oãoJ")]
+lista_bots = [BotMarombeiro("Bambam"), BotGamer("Ninja"), BotEspelhado("oãoJ"), BotMinerin("José"), BotManezinho("Moquirido")]
 
 sys = scb.SistemaChatBot("4FunBots",lista_bots)
 sys.inicio()
