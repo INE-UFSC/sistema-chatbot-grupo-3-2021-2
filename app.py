a186305889c2e0bd7624d4803dbb701fa1a2e6d8
Thiago Bewiahn <thiagoaugustobewiahn@gmail.com>
2021-11-30 11:58:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotMarombeiro import BotMarombeiro
 from Bots.BotGamer import BotGamer
 from Bots.BotEspelhado import BotEspelhado
+from Bots.BotZangado import BotZangado
+from Bots.BotTriste import BotTriste
+from Bots.BotMusical import BotMusical
+from Bots.BotJose import BotJose
+from Bots.BotManezinho import BotManezinho
+from Bots.BotMinerin import BotMinerin
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotMarombeiro("Bambam"), BotGamer("Ninja"), BotEspelhado("oãoJ")]
+lista_bots = [BotMarombeiro("Bambam"), BotGamer("Ninja"), BotEspelhado("oãoJ"), BotMinerin("Renato"), BotManezinho("Moquirido"), BotZangado("Tisca"), BotTriste("Sadboy"), BotMusical("Thiaguinho"), BotJose("José")]
 
 sys = scb.SistemaChatBot("4FunBots",lista_bots)
 sys.inicio()
